19d6e8a4952688d0d942e195067e4d6784c05405
iohan <iohaneduardo1@gmail.com>
2022-02-16 15:00:09 -0300

    base

--- Pastelaria.WindowsApp/MainFrameForm.Designer.cs
+++ Pastelaria.WindowsApp/MainFrameForm.Designer.cs
@@ -29,17 +29,28 @@
         private void InitializeComponent()
         {
             this.panelMenu = new System.Windows.Forms.Panel();
+            this.logoPanel = new System.Windows.Forms.Panel();
+            this.logoPic = new System.Windows.Forms.PictureBox();
+            this.menuBtn = new FontAwesome.Sharp.IconButton();
+            this.logutBtn = new FontAwesome.Sharp.IconButton();
+            this.homeBtn = new FontAwesome.Sharp.IconButton();
             this.panelTitleBar = new System.Windows.Forms.Panel();
+            this.titleLable = new System.Windows.Forms.Label();
             this.minimizeBtn = new FontAwesome.Sharp.IconButton();
             this.maximaziBtn = new FontAwesome.Sharp.IconButton();
             this.exitBtn = new FontAwesome.Sharp.IconButton();
-            this.minimizeBtn2 = new FontAwesome.Sharp.IconButton();
+            this.panelMenu.SuspendLayout();
+            this.logoPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.logoPic)).BeginInit();
             this.panelTitleBar.SuspendLayout();
             this.SuspendLayout();
             // 
             // panelMenu
             // 
             this.panelMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(101)))), ((int)(((byte)(112)))), ((int)(((byte)(184)))));
+            this.panelMenu.Controls.Add(this.logoPanel);
+            this.panelMenu.Controls.Add(this.logutBtn);
+            this.panelMenu.Controls.Add(this.homeBtn);
             this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
             this.panelMenu.ForeColor = System.Drawing.Color.Crimson;
             this.panelMenu.Location = new System.Drawing.Point(0, 0);
@@ -47,13 +58,88 @@
             this.panelMenu.Size = new System.Drawing.Size(250, 681);
             this.panelMenu.TabIndex = 0;
             // 
+            // logoPanel
+            // 
+            this.logoPanel.Controls.Add(this.logoPic);
+            this.logoPanel.Controls.Add(this.menuBtn);
+            this.logoPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.logoPanel.Location = new System.Drawing.Point(0, 0);
+            this.logoPanel.Name = "logoPanel";
+            this.logoPanel.Size = new System.Drawing.Size(250, 140);
+            this.logoPanel.TabIndex = 1;
+            // 
+            // logoPic
+            // 
+            this.logoPic.Location = new System.Drawing.Point(0, 0);
+            this.logoPic.Name = "logoPic";
+            this.logoPic.Size = new System.Drawing.Size(170, 140);
+            this.logoPic.TabIndex = 6;
+            this.logoPic.TabStop = false;
+            // 
+            // menuBtn
+            // 
+            this.menuBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.menuBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(101)))), ((int)(((byte)(112)))), ((int)(((byte)(184)))));
+            this.menuBtn.FlatAppearance.BorderSize = 0;
+            this.menuBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.menuBtn.IconChar = FontAwesome.Sharp.IconChar.Bars;
+            this.menuBtn.IconColor = System.Drawing.Color.Black;
+            this.menuBtn.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.menuBtn.IconSize = 50;
+            this.menuBtn.Location = new System.Drawing.Point(176, 37);
+            this.menuBtn.Name = "menuBtn";
+            this.menuBtn.Size = new System.Drawing.Size(70, 50);
+            this.menuBtn.TabIndex = 1;
+            this.menuBtn.Tag = "Menu";
+            this.menuBtn.UseVisualStyleBackColor = false;
+            this.menuBtn.Click += new System.EventHandler(this.menuBtn_Click);
+            // 
+            // logutBtn
+            // 
+            this.logutBtn.FlatAppearance.BorderSize = 0;
+            this.logutBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.logutBtn.Font = new System.Drawing.Font("Yu Gothic UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.logutBtn.ForeColor = System.Drawing.Color.Black;
+            this.logutBtn.IconChar = FontAwesome.Sharp.IconChar.DoorOpen;
+            this.logutBtn.IconColor = System.Drawing.Color.Black;
+            this.logutBtn.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.logutBtn.IconSize = 50;
+            this.logutBtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.logutBtn.Location = new System.Drawing.Point(0, 624);
+            this.logutBtn.Name = "logutBtn";
+            this.logutBtn.Size = new System.Drawing.Size(250, 45);
+            this.logutBtn.TabIndex = 3;
+            this.logutBtn.Tag = "Logout";
+            this.logutBtn.Text = "Logout";
+            this.logutBtn.UseVisualStyleBackColor = true;
+            // 
+            // homeBtn
+            // 
+            this.homeBtn.FlatAppearance.BorderSize = 0;
+            this.homeBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.homeBtn.Font = new System.Drawing.Font("Yu Gothic UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.homeBtn.ForeColor = System.Drawing.Color.Black;
+            this.homeBtn.IconChar = FontAwesome.Sharp.IconChar.Home;
+            this.homeBtn.IconColor = System.Drawing.Color.Black;
+            this.homeBtn.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.homeBtn.IconSize = 50;
+            this.homeBtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.homeBtn.Location = new System.Drawing.Point(0, 145);
+            this.homeBtn.Margin = new System.Windows.Forms.Padding(0);
+            this.homeBtn.Name = "homeBtn";
+            this.homeBtn.Size = new System.Drawing.Size(250, 45);
+            this.homeBtn.TabIndex = 2;
+            this.homeBtn.Tag = "Home";
+            this.homeBtn.Text = "Home";
+            this.homeBtn.UseVisualStyleBackColor = false;
+            // 
             // panelTitleBar
             // 
             this.panelTitleBar.BackColor = System.Drawing.Color.White;
+            this.panelTitleBar.Controls.Add(this.titleLable);
             this.panelTitleBar.Controls.Add(this.minimizeBtn);
             this.panelTitleBar.Controls.Add(this.maximaziBtn);
             this.panelTitleBar.Controls.Add(this.exitBtn);
-            this.panelTitleBar.Controls.Add(this.minimizeBtn2);
             this.panelTitleBar.Dock = System.Windows.Forms.DockStyle.Top;
             this.panelTitleBar.Location = new System.Drawing.Point(250, 0);
             this.panelTitleBar.Name = "panelTitleBar";
@@ -61,10 +147,22 @@
             this.panelTitleBar.TabIndex = 1;
             this.panelTitleBar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panelTitleBar_MouseDown);
             // 
+            // titleLable
+            // 
+            this.titleLable.AutoSize = true;
+            this.titleLable.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.titleLable.Font = new System.Drawing.Font("Yu Gothic UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.titleLable.Location = new System.Drawing.Point(-1, 6);
+            this.titleLable.Name = "titleLable";
+            this.titleLable.Size = new System.Drawing.Size(146, 37);
+            this.titleLable.TabIndex = 0;
+            this.titleLable.Text = "DashBorad";
+            // 
             // minimizeBtn
             // 
             this.minimizeBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.minimizeBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(101)))), ((int)(((byte)(112)))), ((int)(((byte)(184)))));
+            this.minimizeBtn.FlatAppearance.BorderSize = 0;
             this.minimizeBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.minimizeBtn.IconChar = FontAwesome.Sharp.IconChar.Minus;
             this.minimizeBtn.IconColor = System.Drawing.Color.Black;
@@ -73,7 +171,7 @@
             this.minimizeBtn.Location = new System.Drawing.Point(894, 0);
             this.minimizeBtn.Name = "minimizeBtn";
             this.minimizeBtn.Size = new System.Drawing.Size(40, 25);
-            this.minimizeBtn.TabIndex = 4;
+            this.minimizeBtn.TabIndex = 0;
             this.minimizeBtn.UseVisualStyleBackColor = false;
             this.minimizeBtn.Click += new System.EventHandler(this.minimizeBtn_Click);
             // 
@@ -81,6 +179,7 @@
             // 
             this.maximaziBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.maximaziBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(73)))), ((int)(((byte)(120)))));
+            this.maximaziBtn.FlatAppearance.BorderSize = 0;
             this.maximaziBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.maximaziBtn.IconChar = FontAwesome.Sharp.IconChar.Square;
             this.maximaziBtn.IconColor = System.Drawing.Color.Black;
@@ -89,7 +188,7 @@
             this.maximaziBtn.Location = new System.Drawing.Point(934, 0);
             this.maximaziBtn.Name = "maximaziBtn";
             this.maximaziBtn.Size = new System.Drawing.Size(40, 25);
-            this.maximaziBtn.TabIndex = 3;
+            this.maximaziBtn.TabIndex = 0;
             this.maximaziBtn.UseVisualStyleBackColor = false;
             this.maximaziBtn.Click += new System.EventHandler(this.maximaziBtn_Click);
             // 
@@ -97,6 +196,7 @@
             // 
             this.exitBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.exitBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(97)))), ((int)(((byte)(101)))));
+            this.exitBtn.FlatAppearance.BorderSize = 0;
             this.exitBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.exitBtn.IconChar = FontAwesome.Sharp.IconChar.Plus;
             this.exitBtn.IconColor = System.Drawing.Color.Black;
@@ -106,24 +206,9 @@
             this.exitBtn.Name = "exitBtn";
             this.exitBtn.Rotation = 45D;
             this.exitBtn.Size = new System.Drawing.Size(40, 25);
-            this.exitBtn.TabIndex = 2;
+            this.exitBtn.TabIndex = 0;
             this.exitBtn.UseVisualStyleBackColor = false;
             this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
-            // 
-            // minimizeBtn2
-            // 
-            this.minimizeBtn2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.minimizeBtn2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(101)))), ((int)(((byte)(112)))), ((int)(((byte)(184)))));
-            this.minimizeBtn2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.minimizeBtn2.IconChar = FontAwesome.Sharp.IconChar.Minus;
-            this.minimizeBtn2.IconColor = System.Drawing.Color.Black;
-            this.minimizeBtn2.IconFont = FontAwesome.Sharp.IconFont.Auto;
-            this.minimizeBtn2.IconSize = 15;
-            this.minimizeBtn2.Location = new System.Drawing.Point(894, 0);
-            this.minimizeBtn2.Name = "minimizeBtn2";
-            this.minimizeBtn2.Size = new System.Drawing.Size(40, 25);
-            this.minimizeBtn2.TabIndex = 0;
-            this.minimizeBtn2.UseVisualStyleBackColor = false;
             // 
             // MainFrameForm
             // 
@@ -133,9 +218,15 @@
             this.ClientSize = new System.Drawing.Size(1264, 681);
             this.Controls.Add(this.panelTitleBar);
             this.Controls.Add(this.panelMenu);
+            this.MinimumSize = new System.Drawing.Size(1280, 720);
             this.Name = "MainFrameForm";
             this.Text = "MainFrameForm";
+            this.Resize += new System.EventHandler(this.MainFrameForm_Resize);
+            this.panelMenu.ResumeLayout(false);
+            this.logoPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.logoPic)).EndInit();
             this.panelTitleBar.ResumeLayout(false);
+            this.panelTitleBar.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -144,9 +235,14 @@
 
         private System.Windows.Forms.Panel panelMenu;
         private System.Windows.Forms.Panel panelTitleBar;
-        private FontAwesome.Sharp.IconButton minimizeBtn2;
         private FontAwesome.Sharp.IconButton exitBtn;
         private FontAwesome.Sharp.IconButton maximaziBtn;
         private FontAwesome.Sharp.IconButton minimizeBtn;
+        private System.Windows.Forms.Label titleLable;
+        private FontAwesome.Sharp.IconButton logutBtn;
+        private FontAwesome.Sharp.IconButton homeBtn;
+        private System.Windows.Forms.Panel logoPanel;
+        private FontAwesome.Sharp.IconButton menuBtn;
+        private System.Windows.Forms.PictureBox logoPic;
     }
 }
--- Pastelaria.WindowsApp/MainFrameForm.cs
+++ Pastelaria.WindowsApp/MainFrameForm.cs
@@ -18,14 +18,83 @@ namespace Pastelaria.WindowsApp
             InitializeComponent();
             this.Padding = new System.Windows.Forms.Padding(2);
             this.BackColor = Color.FromArgb(28, 31, 51);
-            this.minimizeBtn2.Padding = new System.Windows.Forms.Padding(1);
         }
+
+            private Size formSize;
+       
 
         protected override void WndProc(ref Message m)
         {
+
             const int WM_NCCALCSIZE = 0x0083;
-            if (m.Msg == WM_NCCALCSIZE && m.WParam.ToInt32() == 1)
+            const int WM_SYSCOMMAND = 0x0112;
+            const int SC_MINIMIZE = 0xF020; 
+            const int SC_RESTORE = 0xF120;
+            const int WM_NCHITTEST = 0x0084;
+            const int resizeAreaSize = 10;
+            #region Form Resize
+
+            const int HTCLIENT = 1;
+            const int HTLEFT = 10;
+            const int HTRIGHT = 11;
+            const int HTTOP = 12;
+            const int HTTOPLEFT = 13;
+            const int HTTOPRIGHT = 14;
+            const int HTBOTTOM = 15;
+            const int HTBOTTOMLEFT = 16;
+            const int HTBOTTOMRIGHT = 17;
+            
+            if (m.Msg == WM_NCHITTEST)
+            {
+                base.WndProc(ref m);
+                if (this.WindowState == FormWindowState.Normal)
+                {
+                    if ((int)m.Result == HTCLIENT)
+                    {
+                        Point screenPoint = new Point(m.LParam.ToInt32());                        
+                        Point clientPoint = this.PointToClient(screenPoint);
+                        if (clientPoint.Y <= resizeAreaSize)
+                        {
+                            if (clientPoint.X <= resizeAreaSize)
+                                m.Result = (IntPtr)HTTOPLEFT;
+                            else if (clientPoint.X < (this.Size.Width - resizeAreaSize))
+                                m.Result = (IntPtr)HTTOP;
+                            else
+                                m.Result = (IntPtr)HTTOPRIGHT;
+                        }
+                        else if (clientPoint.Y <= (this.Size.Height - resizeAreaSize))
+                        {
+                            if (clientPoint.X <= resizeAreaSize)
+                                m.Result = (IntPtr)HTLEFT;
+                            else if (clientPoint.X > (this.Width - resizeAreaSize))
+                                m.Result = (IntPtr)HTRIGHT;
+                        }
+                        else
+                        {
+                            if (clientPoint.X <= resizeAreaSize)
+                                m.Result = (IntPtr)HTBOTTOMLEFT;
+                            else if (clientPoint.X < (this.Size.Width - resizeAreaSize))
+                                m.Result = (IntPtr)HTBOTTOM;
+                            else
+                                m.Result = (IntPtr)HTBOTTOMRIGHT;
+                        }
+                    }
+                }
                 return;
+            }
+            #endregion
+            if (m.Msg == WM_NCCALCSIZE && m.WParam.ToInt32() == 1)
+            {
+                return;
+            }
+            if (m.Msg == WM_SYSCOMMAND)
+            {
+                int wParam = (m.WParam.ToInt32() & 0xFFF0);
+                if (wParam == SC_MINIMIZE)
+                    formSize = this.ClientSize;
+                if (wParam == SC_RESTORE)
+                    this.Size = formSize;
+            }
             base.WndProc(ref m);
         }
 
@@ -54,6 +123,62 @@ namespace Pastelaria.WindowsApp
         private void exitBtn_Click(object sender, EventArgs e)
         {
             Application.Exit();
+        }
+
+        private void MainFrameForm_Resize(object sender, EventArgs e)
+        {
+            AdjustForm();
+        }
+
+        private void AdjustForm() 
+        {
+            switch (this.WindowState)
+            {
+                case FormWindowState.Maximized: 
+                    this.Padding = new Padding(8, 8, 8, 0);
+                    break;
+                case FormWindowState.Normal:
+                    if (this.Padding.Top != 2)
+                        this.Padding = new Padding(2);
+                    break;
+            }
+        }
+
+        private void menuBtn_Click(object sender, EventArgs e)
+        {
+            CollapseMenu();
+        }
+
+        private void CollapseMenu()
+        {
+            if (this.panelMenu.Width > 200)
+            {
+                panelMenu.Width = 120;
+                logoPic.Visible = false;
+                menuBtn.Dock = DockStyle.Top;
+                logoPanel.Height = 50;
+                foreach (Button menuButton in panelMenu.Controls.OfType<Button>())
+                {
+                    menuButton.Text = "";
+                    menuButton.ImageAlign = ContentAlignment.MiddleCenter;
+                    menuButton.Padding = new Padding(0);
+                    menuButton.Width = 120;
+                }
+            }
+            else
+            {
+                panelMenu.Width = 250;
+                logoPic.Visible = true;
+                menuBtn.Dock = DockStyle.None;
+                logoPanel.Height = 140;
+                foreach (Button menuButton in panelMenu.Controls.OfType<Button>())
+                {
+                    menuButton.Text = "   " + menuButton.Tag.ToString();
+                    menuButton.ImageAlign = ContentAlignment.MiddleLeft;
+                    menuButton.Padding = new Padding(10, 0, 0, 0);
+                    menuButton.Width = 250;
+                }
+            }
         }
     }
 }
